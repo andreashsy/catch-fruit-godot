b5759ae1d1da7353c152e92d1a4a2054af7cdf16
unknown <songyanghan+git@gmail.com>
2023-11-11 00:10:46 +0800

    add basket script

---
+++ Basket.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0, 0)
+var score = 0
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -300
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = 300
+	
+	move_and_slide(velocity)
+	velocity.x = lerp(velocity.x, 0, 0.1)
