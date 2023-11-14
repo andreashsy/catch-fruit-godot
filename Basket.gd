9aaf4a5b7a1a446cea6a2d8bca7ba8a33e175af0
unknown <songyanghan+git@gmail.com>
2023-11-14 21:43:35 +0800

    clamp basket to within screen

--- Basket.gd
+++ Basket.gd
@@ -2,15 +2,18 @@ extends KinematicBody2D
 
 var velocity = Vector2(0, 0)
 var score = 0
+var screen_size
 
 func _ready():
-	pass
+	screen_size = get_viewport_rect().size
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -300
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = 300
 	
-	move_and_slide(velocity)
 	velocity.x = lerp(velocity.x, 0, 0.1)
+	position += velocity * delta
+	
+	position.x = clamp(position.x, 0, screen_size.x)
